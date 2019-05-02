289f41234a37612e3f14c80137c3d2f3d14a63aa
Jordan Hale <jordancaseyhale@gmail.com>
2019-05-02 23:21:35 +0100

    First attempt of working simulation

--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,17 @@
+function nextFrame() {
+	if (loop > 10) {
+		clear();
+		draw();
+		loop=0;
+	}
+	else {
+		loop+=1;
+	}
+	reqID = requestAnimationFrame(nextFrame);
+}
+
 function draw() {
+	drawBackground();
 	calculateValues();
 	context.beginPath();
 		context.moveTo(xStart, yStart);
@@ -8,14 +21,14 @@ function draw() {
 		context.moveTo(x1, y1+20);
 		context.lineTo(x2, y2);
 		context.moveTo(x2+10, y2+10);
-		context.arc(x1, y2+10, 10, 0, Math.PI*2, true);
+		context.arc(x2, y2+10, 10, 0, Math.PI*2, true);
 	context.stroke();
 }
 
 function calculateValues() {
 
 	//Calculations for angle 1 acceleration
-	n1 = -g * (2 * m1 * m2) * Math.sin(a1);
+	n1 = -g * (2 * m1 + m2) * Math.sin(a1);
 	n2 = -m2 * g * Math.sin(a1-2*a2);
 	n3 = -2 * Math.sin(a1-a2) * m2;
 	n4 = a2_v*a2_v*r2+a1_v*a1_v*r1*Math.cos(a1-a2);
@@ -30,48 +43,47 @@ function calculateValues() {
 	den = r2 * (2*m1+m2-m2*Math.cos(2*a1-2*a2));
 	a2_a = (n1*(n2+n3+n4))/den;	
 
-	console.log(a1_v);
 	a1_v += a1_a;
 	a2_v += a2_a;
 	a1 += a1_v;
 	a2 += a2_v;
 
-
 	x1 = xStart + r1 * Math.sin(a1);
 	y1 = yStart + r1 * Math.cos(a1);
 	x2 = x1 + r2 * Math.sin(a2);
 	y2 = y1 + r2 * Math.cos(a2);
-	console.log(a1);
-	console.log(Math.sin(Math.PI/2));
-	console.log(x1);
-	console.log(y1);
-	console.log(x2);
-	console.log(y2);
+}
+
+function drawBackground() {
+	context.beginPath();
+		context.rect(0,0,1000,600);
+	context.stroke();
+}
+
+function clear() {
+	context.clearRect(0,0,1000,600);
 }
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext("2d");
-context.beginPath();
-	context.moveTo(200, 200);
-	context.lineTo(500,500);
-	context.rect(0,0,1000,600);
-context.stroke();
 
 var xStart = 500;
-var yStart = 100;
+var yStart = 200;
 var x1, y1, x2, y2, a1, a2;
 var a1_a, a2_a, a1_v, a2_v;
 
 var n1, n2, n3, n4, den;
 
+var loop = 0;
+
 x1 = 100;
 y1 = 100;
 
-var a1 = 0;
-var a2 = 0;
+var a1 = Math.PI/2;
+var a2 = Math.PI/8;
 
-var a1_v = 0;
-var a2_v = 0;
+var a1_v = 0.1;
+var a2_v = 0.1;
 
 var g = 9.81;
 
@@ -84,5 +96,6 @@ var m1 = 20;
 var m2 = 20;
 
 context.strokeStyle = "rgb(100,100,100)";
-var reqID = requestAnimationFrame(draw);
+var reqID = requestAnimationFrame(nextFrame);
+var timing;
 
